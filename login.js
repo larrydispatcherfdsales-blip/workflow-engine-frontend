11de5aaffbcdb385bf2a84cb79560d38061bfc4c
larrydispatcherfdsales-blip <larry.dispatcher.fdsales@gmail.com>
2025-09-18 22:08:02 +0500

    Create login.js

---
+++ login.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const loginButton = document.getElementById('loginButton');
+
+    // Is Client ID ko hum agle step mein haasil karenge.
+    const GITHUB_CLIENT_ID = 'YAHAN_CLIENT_ID_AAYEGA';
+
+    const loginUrl = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=read:user,user:email`;
+
+    loginButton.setAttribute('href', loginUrl );
+});
